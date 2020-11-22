6e9d518cfb7f926bd2e00c04e9a776ee97fb8958
Konstantinos Drakopoulos <drakopuloskostas@gmail.com>
2020-11-22 21:55:42 +0200

    rewrote code to avoid filling memory with images

--- extract_faces.py
+++ extract_faces.py
@@ -33,62 +33,49 @@ face_outputs = Path('tmp_faces/')
 face_outputs.mkdir(parents=True, exist_ok=True)
 
 img_paths = list(map(str, img_dir.rglob("*.jpg"))) + list(map(str, img_dir.rglob("*.jpeg")))
+
 convert_images = lambda path: cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB)
-images = Parallel(n_jobs=-1)(delayed(convert_images)(i) for i in img_paths)
 
 detector = MTCNN()
 
-outputs = Parallel(n_jobs=-1)(delayed(detector.detect_faces)(i) for i in images)
-
-# jdump(outputs, open(embeddings_dir/'pure_out.json', 'w'), indent=4)
-outputs = {k: {'faces': v, 'resolution': list(i.shape[:-1])[::-1]} for k, v, i in zip(img_paths, outputs, images)}
-for k, v in outputs.items():
-    outputs[k] = {}
-    outputs[k]['face'] = get_closest_to_coord_face(
-        faces = v['faces'],
-        x_center = x_center * v['resolution'][1],
-        y_center = y_center * v['resolution'][0]
+def crop_image(pt):
+    # detector = MTCNN()
+    fn = Path(str(pt).replace(str(img_dir), str(embeddings_dir)))
+    if fn.exists():
+        return None
+    image = convert_images(pt)
+    resolution = list(image.shape[:-1])[::-1]
+    detection = detector.detect_faces(image)
+    for face in detection:
+        if len(face["keypoints"]["nose"]) != 2:
+            return {str(pt): {'face': [], 'resolution': resolution}}
+    
+    face = get_closest_to_coord_face(
+        faces = detection,
+        x_center = x_center * resolution[1],
+        y_center = y_center * resolution[0]
     )
-    outputs[k]['resolution'] = v['resolution']
-
-jdump(outputs, open(embeddings_dir/'out.json', 'w'), indent=4)
-
-for k, v in outputs.items():
-    img_idx = img_paths.index(k)
-    face = v['face']
-    resolution = v['resolution']
     eyes = np.array([face['keypoints']['left_eye'], face['keypoints']['right_eye']])
-    buf = np.std(eyes/resolution, axis=0)
-    buf[1] = buf[1]*4
-    buf = (buf * resolution).astype(int)
+    buf = np.std(eyes).astype(int)
+    # buf[0] = buf[0]/2
+    # buf[1] = buf[1]*10
+    # buf = (buf * resolution).astype(int)
     xy_min, xy_max = eyes.min(axis=0) - buf, eyes.max(axis=0) + buf
-    cropped_img = images[img_idx][xy_min[1]:xy_max[1], xy_min[0]:xy_max[0]]
+    cropped_img = image[xy_min[1]:xy_max[1], xy_min[0]:xy_max[0]]
     # fn = 'cropped_' + Path(k[::-1].replace('.', f'.{it}_', 1)[::-1]).name
-    fn = Path(str(k).replace(str(img_dir), str(embeddings_dir)))
+    fn = Path(str(pt).replace(str(img_dir), str(embeddings_dir)))
     if not fn.parent.exists():
         fn.parent.mkdir(parents=True, exist_ok=True)
-    print(f'Cropped {k} in {fn}.')
+    print(f'Cropped {pt} in {fn}.')
     try:
         cv2.imwrite(str(fn), cv2.cvtColor(cropped_img, cv2.COLOR_RGB2BGR))
     except:
         print(f"Error writing {embeddings_dir/fn}")
-    # bounding_box = face['box']
-    # keypoints = face['keypoints']
-    # image = deepcopy(images[img_idx])
-    # fn = Path(k[::-1].replace('.', f'.{it}_', 1)[::-1]).name
+    return {str(pt): {'face': face, 'resolution': resolution}}
 
-    # cv2.rectangle(image,
-    #             (bounding_box[0], bounding_box[1]),
-    #             (bounding_box[0]+bounding_box[2], bounding_box[1] + bounding_box[3]),
-    #             (0,155,255),
-    #             2)
+output = Parallel(n_jobs=-1, require='sharedmem')(delayed(crop_image)(pt) for pt in img_paths)
+outputs = {}
+for out in output:
+    outputs.update(out)
 
-    # cv2.circle(image,(keypoints['left_eye']), 2, (0,155,255), 2)
-    # cv2.circle(image,(keypoints['right_eye']), 2, (0,155,255), 2)
-    # cv2.circle(image,(keypoints['nose']), 2, (0,155,255), 2)
-    # cv2.circle(image,(keypoints['mouth_left']), 2, (0,155,255), 2)
-    # cv2.circle(image,(keypoints['mouth_right']), 2, (0,155,255), 2)
-    # try:
-    #     cv2.imwrite(str(embeddings_dir/fn), cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
-    # except:
-    #     print(f"Error writing {embeddings_dir/fn}")
+jdump(outputs, open(embeddings_dir/'out.json', 'w'), indent=4)
