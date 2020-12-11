04746f6153bcc7d7f89830040b9d025a69ff4f96
Konstantinos Drakopoulos <drakopuloskostas@gmail.com>
2020-12-11 17:25:02 +0200

    conversion to frames

--- convert_video_to_frames.py
+++ convert_video_to_frames.py
@@ -1,10 +1,14 @@
+from types import FrameType
 import cv2
+import pandas as pd
+from pandas.core import frame
 from psutil import cpu_count
 from joblib import Parallel, delayed
 from pathlib import Path
+from sys import argv
 import os
 
-def extract_frames(video_path, frames_dir, overwrite=False, start=-1, end=-1, every=1):
+def extract_frames(video_path, frames_dir, frame_list=None):
     """
     Extract frames from a video using OpenCVs VideoCapture
 
@@ -25,9 +29,11 @@ def extract_frames(video_path, frames_dir, overwrite=False, start=-1, end=-1, ev
 
     capture = cv2.VideoCapture(str(video_path))  # open the video using OpenCV
 
-    if start < 0:  # if start isn't specified lets assume 0
+    if not (frame_list is None):
+        start = frame_list.min()
+        end = frame_list.max()
+    else:
         start = 0
-    if end < 0:  # if end isn't specified assume the end of the video
         end = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
 
     capture.set(1, start)  # set the starting frame of the capture
@@ -37,13 +43,16 @@ def extract_frames(video_path, frames_dir, overwrite=False, start=-1, end=-1, ev
 
     save_dir = frames_dir/f'{(frame_no//1000):03d}'/f'{(frame_no%1000//100)}'
     save_dir.mkdir(parents=True, exist_ok=True)
-
+    every = 1
     while frame_no < end:  # lets loop through the frames until the end
+        success, image = capture.read()  # read an image from the capture
+        if not (frame_no in frame_list):
+            frame_no += 1
+            continue
+
         if frame_no % 100 == 0:
             save_dir = frames_dir/f'{(frame_no//1000):03d}'/f'{(frame_no%1000//100)}'
             save_dir.mkdir(parents=True, exist_ok=True)
-
-        _, image = capture.read()  # read an image from the capture
 
         if while_safety > 500:  # break the while if our safety maxs out at 500
             break
@@ -67,16 +76,29 @@ def extract_frames(video_path, frames_dir, overwrite=False, start=-1, end=-1, ev
     return saved_count  # and return the count of the images we saved
 
 if __name__ == '__main__':
+    input_csv = pd.read_csv(argv[1], index_col=0).reset_index(drop=True)
+    frame_list = input_csv.groupby('file').frame.unique()
     videos = list(Path('videos/').glob('*'))
     output = Path('images/')
     num_processes = cpu_count()
     if len(videos) < num_processes:
         num_processes = len(videos)
-    frame_dirs = [output/i.stem for i in videos]
+    existing_frames = {}
+    non_existing = []
+    existing_videos = []
+    for v in videos:
+        if v.name in frame_list.index:
+            existing_frames[v.name] = frame_list[v.name]
+            existing_videos.append(v)
+        else:
+            non_existing.append(str(v))
+    frame_dirs = [output/i.stem for i in existing_videos]    
     for d in frame_dirs:
         d.mkdir(parents=True, exist_ok=True)
-    frame_counts = Parallel(n_jobs=num_processes)(delayed(extract_frames)(video, output_dir) for video, output_dir in zip(videos, frame_dirs))
+    # frame_counts = []
+    frame_counts = Parallel(n_jobs=num_processes)(delayed(extract_frames)(video, output_dir, existing_frames[video.name]) for video, output_dir in zip(existing_videos, frame_dirs))
     # frame_counts = [extract_frames(video, output_dir) for video, output_dir in zip(videos, frame_dirs)]
     print('Success!')
-    for i, v in enumerate(videos):
+    for i, v in enumerate(existing_videos):
         print(f'{v.name}\t{frame_counts[i]} frames written to {frame_dirs[i]}')
+    print('Could not process:' + "\n\t".join(non_existing))
